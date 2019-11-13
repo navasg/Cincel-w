fd07f98e5eb6bf3725e27550feaf430aadf13b59
navasg <genenavas97@gmail.com>
2019-11-13 10:50:07 -0400

    Add section about

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -214,3 +214,28 @@ $('.main-clients__slider-nav').slick({
   infinite: true,
   focusOnSelect: true
 });
+
+$('.main-about__carousel').slick({
+  centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [{
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});

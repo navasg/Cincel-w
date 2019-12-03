903c5662000df40ec611b58295010a059cb101f9
navasg <genenavas97@gmail.com>
2019-12-03 12:11:44 -0400

    otras vistas

--- wp-config.php
+++ wp-config.php
@@ -18,15 +18,24 @@
  * @package WordPress
  */
 
+ define( 'DB_NAME', 'nombredetubd' );
+
+ /** MySQL database username */
+ define( 'DB_USER', 'root' );
+ 
+ /** MySQL database password */
+ define( 'DB_PASSWORD', '1010Will*_' );
+ 
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'Cincel' );
 
 /** MySQL database username */
-define( 'DB_USER', 'admin' );
+// define( 'DB_USER', 'admin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '1234' );
+// define( 'DB_PASSWORD', '1234' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
